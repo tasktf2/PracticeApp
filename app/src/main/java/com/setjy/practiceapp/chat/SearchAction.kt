626c3590fa879699f59214d292245bf168aa76a8
setJy <denis.mashkov@flexsentlabs.com>
2022-07-14 16:43:25 +0500

    Add SearchAction, SearchMessagesIterator, RxJavaUtils, move ChatFragment to chat package, fix magic numbers, refactor ChatFragment

---
+++ app/src/main/java/com/setjy/practiceapp/chat/SearchAction.kt
@@ -0,0 +1,8 @@
+package com.setjy.practiceapp.chat
+
+enum class SearchAction {
+    START,
+    NEXT,
+    PREV,
+    CANCEL
+}
---
+++ app/src/main/java/com/setjy/practiceapp/util/RxJavaUtils.kt
@@ -0,0 +1,8 @@
+package com.setjy.practiceapp.util
+
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+
+operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
+    this.add(disposable)
+}
