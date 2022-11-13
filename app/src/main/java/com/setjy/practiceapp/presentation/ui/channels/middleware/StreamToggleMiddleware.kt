0d5bc296b7b2f705ad985d5742d1f9935917c67f
setJy <denis.mashkov@flexsentlabs.com>
2022-11-13 18:36:06 +0500

    minor changes

--- app/src/main/java/com/setjy/practiceapp/presentation/ui/channels/middleware/StreamToggleMiddleware.kt
+++ app/src/main/java/com/setjy/practiceapp/presentation/ui/channels/middleware/StreamToggleMiddleware.kt
@@ -12,23 +12,17 @@ class StreamToggleMiddleware : Middleware<ChannelsState, ChannelsAction> {
     ): Observable<ChannelsAction> {
         return actions.ofType(ChannelsAction.ToggleStream::class.java)
             .withLatestFrom(state) { action, lastState ->
-                ChannelsAction.ShowToggleStream(
-                    streams = lastState.streams.orEmpty()
-                        .map { stream ->
-                            if (stream.streamId == action.stream.streamId) {
-                                stream.copy(isExpanded = !stream.isExpanded)
-                            } else {
-                                stream
-                            }
-                        },
-                    items = lastState.streams.orEmpty()
-                        .map { stream ->
-                            if (stream.streamId == action.stream.streamId) {
-                                stream.copy(isExpanded = !stream.isExpanded)
-                            } else {
-                                stream
-                            }
+                val streams = lastState.streams.orEmpty()
+                    .map { stream ->
+                        if (stream.streamId == action.stream.streamId) {
+                            stream.copy(isExpanded = !stream.isExpanded)
+                        } else {
+                            stream
                         }
+                    }
+                ChannelsAction.ShowToggleStream(
+                    streams = streams,
+                    items = streams
                         .flatMap { stream ->
                             if (stream.isExpanded) {
                                 listOf(stream) + stream.listOfTopics
